64db8331b3e2d27399357d8a96f550db5e2ac3b7
tommaso auerbach <tsa302@nyu.edu>
2019-05-31 22:24:57 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,23 @@
+
 def unsafe?(speed)
+
+	if speed > 60
+
+	elsif speed < 40
+
+		puts "safe"
+
+	else
+
+		puts "not safe"
+
+		if speed
 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed <40 ? "safe" : "not safe"
+
 end
-	
-
-
